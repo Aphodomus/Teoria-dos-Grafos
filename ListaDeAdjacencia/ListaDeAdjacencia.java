09f33016be14e3608564d475180484784f7980b4
Daniel Vitor <daniel.santos@brius.com.br>
2022-03-09 07:59:00 -0300

    feat: add breadth first search in java

--- ListaDeAdjacencia/ListaDeAdjacencia.java
+++ ListaDeAdjacencia/ListaDeAdjacencia.java
@@ -3,14 +3,14 @@ import java.util.List;
 
 class Aresta {
     private Vertice next;
-    private float weight;
+    private double weight;
 
     Aresta() {
         this.next = null;
         this.weight = 0;
     }
 
-    Aresta(Vertice dest, float weight) {
+    Aresta(Vertice dest, double weight) {
         this.weight = weight;
         this.next = dest;
     }
@@ -20,16 +20,16 @@ class Aresta {
         this.weight = 1;
     }
 
-    Aresta(float weight) {
+    Aresta(double weight) {
         this.weight = weight;
         this.next = null;
     }
 
-    public float getWeight() {
+    public double getWeight() {
         return this.weight;
     }
 
-    public void setWeight(float weight) {
+    public void setWeight(double weight) {
         this.weight = weight;
     }
 
@@ -45,11 +45,14 @@ class Aresta {
 class Vertice {
     private int label;
     private int grau;
+    private boolean visitado;
     private List<Aresta> arestas;
 
     Vertice (int label) {
         this.label = label;
         this.arestas = new ArrayList<Aresta>();
+        this.grau = 0;
+        this.visitado = false;
     }
 
     public int getLabel() {
@@ -71,6 +74,14 @@ class Vertice {
     public int getDegree() {
         return this.grau;
     }
+
+    public boolean getVisited() {
+        return this.visitado;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visitado = visited;
+    }
 }
 
 class Grafo {
@@ -84,30 +95,42 @@ class Grafo {
         this.numArestas = 0;
     }
 
-    public void inserir(int v1, int v2, float peso) {
-        Vertice vertice1 = new Vertice(v1);
-        Vertice vertice2 = new Vertice(v2);
+    public int getNumVertices() {
+        return this.numVertices;
+    }
 
+    public int getNumArestas() {
+        return this.numArestas;
+    }
+
+    public void inserir(int v1, int v2, double peso) {
         // Verificar se vertice1 já existe no grafo, se não existir criar
-        if (vertices.contains(vertice1) == false) {
-            this.vertices.add(vertice1);
-            this.numVertices++;
-        }
+        Vertice aux1 = this.procurarVertice(v1, peso);
 
         // Verificar se vertice2 já existe no grafo, se não existir criar
-        if (vertices.contains(vertice2) == false) {
-            this.vertices.add(vertice2);
-            this.numVertices++;
-        }
+        Vertice aux2 = this.procurarVertice(v2, peso);
 
         // Obrigatoriamente pegar os vertices da lista para fazer a ligação entre eles
-        Vertice aux1 = this.vertices.get(vertices.indexOf(vertice1));
-        Vertice aux2 = this.vertices.get(vertices.indexOf(vertice2));
-
         this.ligarArestas(aux1, aux2, peso);
     }
 
-    public void ligarArestas(Vertice v1, Vertice v2, float peso) {
+    public Vertice procurarVertice(int v1, double peso) {
+        for (int i = 0; i < this.vertices.size(); i++) {
+            // Se ele encontrar o vertice dentro da lista, apenas retorna-lo
+            if (this.vertices.get(i).getLabel() == v1) {
+                return this.vertices.get(i);
+            }
+        }
+
+        // Se percorrer toda a lista e nao achar o vertice, cria-lo
+        Vertice vertice1 = new Vertice(v1);
+        this.vertices.add(vertice1);
+        numVertices++;
+
+        return vertice1;
+    }
+
+    public void ligarArestas(Vertice v1, Vertice v2, double peso) {
         v1.getEdges().add(new Aresta(v2, peso));
         v1.increaseDegree();
         this.numArestas++;
@@ -123,14 +146,47 @@ class Grafo {
             System.out.print("\n");
         }
     }
+
+    public void breadthFirstSearch(int start) {
+        List<Vertice> fila = new ArrayList<Vertice>();
+        Vertice initial = this.vertices.get(start);
+        initial.setVisited(true); // Marcar o vertice inicial como visitado
+        System.out.println(initial.getLabel());
+        fila.add(initial);
+
+        while(fila.size() > 0) {
+            Vertice currently = fila.get(0); // Pegar o primeiro vertice da fila
+
+            for (int i = 0; i < currently.getEdges().size(); i++) {
+                Vertice adjacent = currently.getEdges().get(i).getNext();
+
+                // Se o vertice não foi visitado, marca-lo
+                if (adjacent.getVisited() == false) {
+                    System.out.println(adjacent.getLabel());
+                    adjacent.setVisited(true);
+                    fila.add(adjacent); // Adicionar o vertice a minha lista para visita-lo depois
+                }
+
+            }
+            fila.remove(0);
+        }
+    }
+
+    public void depthFirstSearch(int start) {
+        
+    }
 }
 
 public class ListaDeAdjacencia {
     public static void main(String[] args) {
         Grafo grafo = new Grafo();
     
-        grafo.inserir(0, 1, 0.45f);
-    
-        grafo.print();
+        grafo.inserir(0, 1, 0.5);
+        grafo.inserir(0, 2, 0.3);
+        grafo.inserir(1, 3, 0.2);
+        grafo.inserir(2, 3, 0.1);
+        grafo.inserir(4, 2, 0.6);
+        grafo.inserir(4, 3, 0.7);
+        grafo.breadthFirstSearch(4);
     }
-}
+} 
